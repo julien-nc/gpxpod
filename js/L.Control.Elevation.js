44e3dba72928816dc3a55b408da52753c5103316
Julien Veyssier <eneiluj@gmx.fr>
2017-01-05 15:57:21 +0100

    fix mouseover in chrome(ium) for elevation chart

--- js/L.Control.Elevation.js
+++ js/L.Control.Elevation.js
@@ -104,7 +104,20 @@ L.Control.Elevation = L.Control.extend({
             .style("stroke", "none")
             .style("pointer-events", "all");
 
-        if (L.Browser.touch) {
+        if (L.Browser.chrome) {
+            background.on("touchmove.drag", this._dragHandler.bind(this)).
+                on("touchstart.drag", this._dragStartHandler.bind(this)).
+                on("touchstart.focus", this._mousemoveHandler.bind(this));
+            L.DomEvent.on(this._container, 'touchend', this._dragEndHandler, this);
+
+            background.on("mousemove.focus", this._mousemoveHandler.bind(this)).
+                on("mouseout.focus", this._mouseoutHandler.bind(this)).
+                on("mousedown.drag", this._dragStartHandler.bind(this)).
+                on("mousemove.drag", this._dragHandler.bind(this));
+            L.DomEvent.on(this._container, 'mouseup', this._dragEndHandler, this);
+
+        }
+        else if (L.Browser.touch) {
 
             background.on("touchmove.drag", this._dragHandler.bind(this)).
             on("touchstart.drag", this._dragStartHandler.bind(this)).
