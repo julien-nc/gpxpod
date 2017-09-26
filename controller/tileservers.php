dc31f1261076fdb09834e3b799b8ab50d30338b8
Julien Veyssier <eneiluj@gmx.fr>
2017-09-26 17:48:47 +0200

    refs #91 correct transport map URL

--- controller/tileservers.php
+++ controller/tileservers.php
@@ -41,7 +41,7 @@ $baseTileServers = [
     Array(
         'name' => 'OpenStreetMap Transport',
         'type' => 'tile',
-        'url' => 'http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png',
+        'url' => 'https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
         'attribution' => 'Map data &copy; 2013 <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         'minzoom' => '1',
         'maxzoom' => '18'
