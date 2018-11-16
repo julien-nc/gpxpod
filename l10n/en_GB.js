1eaac5975de245f98559c0ef0a61fa596b5dbde3
Julien Veyssier <eneiluj@posteo.net>
2018-11-16 12:22:48 +0100

    write new translations

---
+++ l10n/en_GB.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "gpxpod",
+    {
+    "GpxPod" : "GpxPod"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxpod\nX-Crowdin-Language: en-GB");
