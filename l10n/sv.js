1d6962ffe9db3fcbebcd12325078aeb63ee93e0f
Julien Veyssier <eneiluj@posteo.net>
2019-02-19 02:54:13 +0100

    write new translations

---
+++ l10n/sv.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "gpxpod",
+    {
+    "GpxPod" : "GpxPod",
+    "There was an error during \"gpxelevations\" execution on the server" : "Ett fel uppstod under ”gpxelevations” utförande på servern",
+    "View in GpxPod" : "Visa i GpxPod",
+    "File" : "Fil",
+    "download" : "ladda ned",
+    "This public link will work only if '{title}' or one of its parent folder is shared in 'files' app by public link without password" : "Denna publika länk fungerar endast om '{title}' eller en föräldermapp delas i 'files' app genom en publik länk - utan lösenord",
+    "Draw track" : "Rita rutt",
+    "metadata link" : "metadata länk",
+    "tracks/routes name list" : "spår/rutter namn lista",
+    "Distance" : "Distans",
+    "Duration" : "Tid",
+    "Moving time" : "Tid i rörelse",
+    "Pause time" : "Paustid",
+    "Begin" : "Starta",
+    "End" : "Avsluta",
+    "Cumulative elevation gain" : "Total höjdstigning",
+    "Cumulative elevation loss" : "Total höjdminskning"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxpod\nX-Crowdin-Language: sv-SE");
