34acc3d552762e7d525ab8e97a3de99202d2ea4f
Julien Veyssier <eneiluj@posteo.net>
2019-04-12 12:14:33 +0200

    write translations

---
+++ l10n/en_GB.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "gpxpod",
+    {
+    "GpxPod" : "GpxPod"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/pt_PT.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "gpxpod",
+    {
+    "download" : "transferir"
+},
+"nplurals=2; plural=(n != 1);");
