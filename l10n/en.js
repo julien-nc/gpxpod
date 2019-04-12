68f482f1e1b63c715a42a8182eeb36f7c253030d
Julien Veyssier <eneiluj@posteo.net>
2019-04-12 12:02:18 +0200

    update l10n stuff

--- l10n/en.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "gpxpod",
-    {
-    "GpxPod" : "GpxPod"
-},
-"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxpod\nX-Crowdin-Language: en-US");
--- l10n/pt_PT.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "gpxpod",
-    {
-    "download" : "transferir"
-},
-"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxpod\nX-Crowdin-Language: pt-PT");
