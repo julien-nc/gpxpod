6f300ff1d8782626bcd85e5c01443f999c217f43
Julien Veyssier <eneiluj@posteo.net>
2019-12-30 21:37:47 +0100

    new trans files and read strings [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ l10n/it.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "gpxpod",
+    {
+    "GpxPod" : "GpxPod",
+    "There was an error during \"gpxelevations\" execution on the server" : "Si è verificato un errore durante l'esecuzione \"gpxelevations\" sul server"
+},
+"nplurals=2; plural=(n != 1);");
