986c34ddeb2c0fad78a1838bdfaffd93705aedf3
Julien Veyssier <eneiluj@posteo.net>
2020-05-30 16:25:22 +0200

    add CI nightly build target, adapt makefile
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,7 +9,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -yqq
 apt-get install git sudo php-xdebug php7.3-sqlite3 php7.3-curl\
     php7.3-xml php7.3-mbstring php7.3-json php7.3-zip php7.3-gd\
-    php7.3-intl unzip curl wget nodejs npm sed gpsbabel -yqq > /dev/null 2>&1
+    php7.3-intl unzip curl wget nodejs npm sed gpsbabel make rsync -yqq > /dev/null 2>&1
 npm install -g jshint > /dev/null 2>&1
 
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
