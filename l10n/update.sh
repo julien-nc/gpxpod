9d5a1aed82f274a99b77f891562829756b781bfe
Julien Veyssier <eneiluj@posteo.net>
2021-02-07 18:40:56 +0100

    update update.sh
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /tmp/gpxpod
-git clone https://gitlab.com/eneiluj/gpxpod-oc /tmp/gpxpod -b l10n_master
+git clone https://gitlab.com/eneiluj/gpxpod-oc /tmp/gpxpod -b l10n_master --single-branch
 cp -r /tmp/gpxpod/l10n/descriptions/[a-z][a-z]_[A-Z][A-Z] ./descriptions/
 cp -r /tmp/gpxpod/translationfiles/[a-z][a-z]_[A-Z][A-Z] ../translationfiles/
 rm -rf /tmp/gpxpod
