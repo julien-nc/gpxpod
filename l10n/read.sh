6aaa1033f895ed042eb16801d6df1142269e5057
Julien Veyssier <eneiluj@posteo.net>
2021-03-12 11:09:20 +0100

    fix read.sh
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/read.sh
+++ l10n/read.sh
@@ -3,6 +3,8 @@
 cd ..
 rm -rf /tmp/gpxpodjs
 mv js /tmp/gpxpodjs
+mv node_modules /tmp/node_modules_gpxpod
 translationtool.phar create-pot-files
 mv /tmp/gpxpodjs js
+mv /tmp/node_modules_gpxpod node_modules
 cd -
