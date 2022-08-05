28a8509e43dfc7d2c007bdab93b66db3e7c7eee4
Julien Veyssier <eneiluj@posteo.net>
2022-08-05 19:09:45 +0200

    begin to implement point info popup, find the point on hover
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/mixins/MethodOnBorderHover.js
+++ src/mixins/MethodOnBorderHover.js
@@ -1,10 +1,40 @@
+import { LngLat } from 'maplibre-gl'
+
 export default {
 	methods: {
+		findPoint(lngLat) {
+			let minDist = 40000000
+			let minDistPoint = null
+			let tmpDist
+			this.track.geojson.features.forEach((feature) => {
+				if (feature.geometry.type === 'LineString') {
+					feature.geometry.coordinates.forEach(c => {
+						tmpDist = lngLat.distanceTo(new LngLat(c[0], c[1]))
+						if (tmpDist < minDist) {
+							minDist = tmpDist
+							minDistPoint = c
+						}
+					})
+				} else if (feature.geometry.type === 'MultiLineString') {
+					feature.geometry.coordinates.forEach((coords) => {
+						coords.forEach(c => {
+							tmpDist = lngLat.distanceTo(new LngLat(c[0], c[1]))
+							if (tmpDist < minDist) {
+								minDist = tmpDist
+								minDistPoint = c
+							}
+						})
+					})
+				}
+			})
+			console.debug('found', minDistPoint)
+			// TODO display popup with point information
+			// or just report the found point to let the <Map> display a point (for this point: on hover: display info)
+		},
 		onBorderMouseEnter(e) {
 			this.bringToTop()
 			this.map.getCanvas().style.cursor = 'pointer'
-			console.debug('bbbbbbbbbbbbbb', e)
-			// this.$emit('line-hover', e.lngLat)
+			this.findPoint(e.lngLat)
 		},
 		onBorderMouseLeave(e) {
 			this.map.getCanvas().style.cursor = ''
