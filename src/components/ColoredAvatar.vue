0efffffa9e5984d41b5e84c5e0d463dfc3963859
Julien Veyssier <julien-nc@posteo.net>
2023-03-31 15:39:10 +0200

    fix track item avatar text style
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/ColoredAvatar.vue
+++ src/components/ColoredAvatar.vue
@@ -11,6 +11,8 @@
 <script>
 import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
 
+import { hexToRgb } from '../utils.js'
+
 export default {
 	name: 'ColoredAvatar',
 
@@ -42,9 +44,16 @@ export default {
 	computed: {
 		cssVars() {
 			return {
+				'--member-fg-color': this.color === 'gradient' ? 'white' : this.textColor,
 				'--member-bg-color': this.color === 'gradient' ? 'unset' : this.color,
 				'--member-bg-gradient': this.color === 'gradient' ? 'linear-gradient(to right, blue, green, orange, red)' : 'unset',
 			}
+		},
+		textColor() {
+			const rgb = hexToRgb(this.color)
+			return rgb.r + rgb.g + rgb.b > 3 * 128
+				? 'black'
+				: 'white'
 		},
 	},
 
@@ -70,5 +79,12 @@ export default {
 .avatar {
 	background-color: var(--member-bg-color) !important;
 	background-image: var(--member-bg-gradient) !important;
+}
+
+:deep(.avatardiv__initials-wrapper) {
+	div {
+		color: var(--member-fg-color) !important;
+		font-weight: bold !important;
+	}
 }
 </style>
