a180a8496dc041fb49c4712e8ddc751c3c610a64
Julien Veyssier <julien-nc@posteo.net>
2023-06-05 17:38:57 +0200

    do not use OC functions/vars anymore
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/adminSettings.js
+++ src/adminSettings.js
@@ -9,8 +9,11 @@
  * @copyright Julien Veyssier 2022
  */
 
-__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
-__webpack_public_path__ = OC.linkTo('gpxpod', 'js/') // eslint-disable-line
+import { linkTo } from '@nextcloud/router'
+import { getRequestToken } from '@nextcloud/auth'
+
+__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_public_path__ = linkTo('gpxpod', 'js/') // eslint-disable-line
 
 document.addEventListener('DOMContentLoaded', async (event) => {
 	const { default: Vue } = await import(/* webpackChunkName: "admin-settings-lazy" */'vue')
