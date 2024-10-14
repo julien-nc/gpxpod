0ca79413923de747a4c6ba2fad960568c0969f82
Julien Veyssier <julien-nc@posteo.net>
2024-10-14 13:50:47 +0200

    add csp listener to run csp->addAllowedWorkerSrcDomain('blob:')
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -15,11 +15,13 @@ use OCA\Files\Event\LoadAdditionalScriptsEvent;
 
 use OCA\Files_Sharing\Event\BeforeTemplateRenderedEvent;
 use OCA\GpxPod\Listener\AddFilesScriptsListener;
+use OCA\GpxPod\Listener\CSPListener;
 use OCA\GpxPod\Listener\FilesSharingAddScriptsListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\Security\CSP\AddContentSecurityPolicyEvent;
 
 class Application extends App implements IBootstrap {
 
@@ -105,6 +107,7 @@ class Application extends App implements IBootstrap {
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(LoadAdditionalScriptsEvent::class, AddFilesScriptsListener::class);
 		$context->registerEventListener(BeforeTemplateRenderedEvent::class, FilesSharingAddScriptsListener::class);
+		$context->registerEventListener(AddContentSecurityPolicyEvent::class, CSPListener::class);
 	}
 
 	public function boot(IBootContext $context): void {
