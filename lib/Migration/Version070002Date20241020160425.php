472427dad7a608ccab1bc710e51202fd6e33aae7
Julien Veyssier <julien-nc@posteo.net>
2024-10-22 00:07:06 +0200

    fix migration, remove duplicate directories before adding unique constraint
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Migration/Version070002Date20241020160425.php
+++ lib/Migration/Version070002Date20241020160425.php
@@ -6,12 +6,16 @@ namespace OCA\GpxPod\Migration;
 
 use Closure;
 use OCP\DB\ISchemaWrapper;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
 class Version070002Date20241020160425 extends SimpleMigrationStep {
 
-	public function __construct() {
+	public function __construct(
+		private IDBConnection $connection,
+	) {
 	}
 
 	/**
@@ -20,6 +24,29 @@ class Version070002Date20241020160425 extends SimpleMigrationStep {
 	 * @param array $options
 	 */
 	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		$qbDelete = $this->connection->getQueryBuilder();
+		$qbDelete->delete('gpxpod_directories')
+			->where(
+				$qbDelete->expr()->eq('id', $qbDelete->createParameter('idToDelete'))
+			);
+
+		$qbSelect = $this->connection->getQueryBuilder();
+		$qbSelect->select('id', 'user', 'path', $qbSelect->createFunction('COUNT(*)'))
+			->from('gpxpod_directories')
+			->having('COUNT(*) > 1')
+			->groupBy('user', 'path');
+
+		do {
+			$hasDuplicates = false;
+			$selectResult = $qbSelect->executeQuery();
+			while ($row = $selectResult->fetch()) {
+				$hasDuplicates = true;
+				$id = $row['id'];
+				$qbDelete->setParameter('idToDelete', $id, IQueryBuilder::PARAM_INT);
+				$qbDelete->executeStatement();
+			}
+			$selectResult->closeCursor();
+		} while ($hasDuplicates);
 	}
 
 	/**
@@ -34,7 +61,7 @@ class Version070002Date20241020160425 extends SimpleMigrationStep {
 		$schemaChanged = false;
 
 		$table = $schema->getTable('gpxpod_directories');
-		if ($table->hasColumn('user') && $table->hasColumn('path')) {
+		if ($table->hasColumn('user') && $table->hasColumn('path') && !$table->hasIndex('gpxpod_dir_user_path_uniq')) {
 			$table->addUniqueIndex(['user', 'path'], 'gpxpod_dir_user_path_uniq');
 			$schemaChanged = true;
 		}
