9273a2a1cfc617888f24dcbfa62d3ca1d2c47ef6
Julien Veyssier <julien-nc@posteo.net>
2026-06-21 16:36:16 +0200

    run cs:fix
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Controller/MapController.php
+++ lib/Controller/MapController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2015 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 namespace OCA\GpxPod\Controller;
 
 use OCA\GpxPod\Service\MapService;
